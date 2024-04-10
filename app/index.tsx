a576177a3f337d8f622ccd62b3ace35c290080f7
tobywiedenhoefer <toby.wiedenh@gmail.com>
2024-04-09 21:10:18 -0500

    Initialize repo

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { StyleSheet, View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Example</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
